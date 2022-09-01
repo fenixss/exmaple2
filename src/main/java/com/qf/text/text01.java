d6e67266e559080a89407717fb5af3a129d6614b
lipeicheng <1846833697@qq.com>
2022-09-01 17:37:07 +0800

    go
    Signed-off-by: lipeicheng <1846833697@qq.com>

--- src/main/java/com/qf/text/text01.java
+++ src/main/java/com/qf/text/text01.java
@@ -3,5 +3,8 @@ package com.qf.text;
 public class text01 {
     public static void main(String[] args) {
         System.out.println("1231231231");
+        System.out.println("12312312312");
+        System.out.println("14124");
+        System.out.println("look to the stars");
     }
 }
